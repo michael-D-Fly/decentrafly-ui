250ae962312410ed1204577b383f991c802eeb29
Constantin Michael Weisser <i7c@posteo.de>
2023-08-18 08:47:49 -0300

    Login URL environment dependent

--- src/routes/components/navigation.svelte
+++ src/routes/components/navigation.svelte
@@ -1,6 +1,14 @@
 <script>
 	import { token } from '$lib/stores/auth';
 	import { store as user } from '$lib/stores/user';
+
+	let login_client_id="20ljuqkee1ci7n4b1nuomitf6o"
+	let login_flow="token"
+
+	let login_redirect_uri = (import.meta.env.DEV) ? "http://localhost:5173/login" : "https://ui.decentrafly.org/login"
+	let login_base_uri = "https://decentraflytestsignin.auth.us-east-1.amazoncognito.com"
+	let login_uri=`${login_base_uri}/login?client_id=${login_client_id}&redirect_uri=${encodeURI(login_redirect_uri)}&response_type=${login_flow}`
+
 </script>
 
 
@@ -10,7 +18,7 @@
 <nav>
     <a href="/">Home</a>
 	{#if ($user == undefined)}
-	    <a href="https://decentraflytestsignin.auth.us-east-1.amazoncognito.com/login?client_id=20ljuqkee1ci7n4b1nuomitf6o&redirect_uri=https%3A%2F%2Fui.decentrafly.org%2Flogin&response_type=token">Login</a>
+	    <a href="{login_uri}">Login</a>
 	{:else}
 		<a href="/account">Account</a>
 		<a href="#" on:click={() => {token.update((v) => undefined)}}>Logout</a>
